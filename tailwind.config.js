b767ee81c936b14bbc0f6630c45e6acfcbc57c7a
Tadynas <griciustadas@gmail.com>
2022-09-21 10:15:36 +0300

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./sections/**/*.liquid', './snippets/**/*.liquid'],
-  theme: {
-    extend: {
-      height: {
-        '94': '22rem'
-      }
-    },
-  },
+  theme: {},
   plugins: [],
 }
